4c88f7acba03f05d839fff3bf1f8d13843ef4205
rohanambole <rohanambole3452@gmail.com>
2023-01-09 16:37:33 +0530

    fist comiit

---
+++ src/main/java/com/git/Student.java
@@ -0,0 +1,49 @@
+package com.git;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+   private int id;
+	
+	@Column(name="name")
+   private String name;
+	
+	@Column(name="city")
+   private String city;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
---
+++ src/main/java/com/git/Test.java
@@ -0,0 +1,40 @@
+package com.git;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+
+		// step-1
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+
+		// step_2
+		SessionFactory sf = configuration.buildSessionFactory();
+
+		// step3
+		Session session = sf.openSession();
+		
+		//step-4
+		Transaction transaction = session.beginTransaction();
+	
+		//step-5
+		
+	Student std = (Student) session.get(Student.class, 1);
+		
+	std.setName("akshay");
+	std.setCity("nagar");
+	
+	session.update(std);
+		
+		transaction.commit();
+		
+		System.out.println("Record is updated >>>>");
+		
+	}
+}
